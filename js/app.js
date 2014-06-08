42bc592c1cbc53459dc98172074c345c5dddc694
Scott Mascio <scottmascio@gmail.com>
2014-06-08 15:16:22 -0400

    first commit

---
+++ js/app.js
@@ -0,0 +1,10 @@
+App = Ember.Application.create();
+
+App.Router.map(function() {
+  this.resource('resume', { path: '/' });
+  this.resource('spot2fish');
+  this.resource('apprentice');
+  this.resource('referencesection');
+  this.resource('personal');
+  this.resource('resume');
+});
